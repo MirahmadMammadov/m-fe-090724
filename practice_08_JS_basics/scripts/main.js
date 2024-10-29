a5d2525279491ba18f0a209bd1b9199ef4672028
Tel-ran.de <mizahmadm@gmail.com>
2024-10-29 10:22:29 +0100

    m-fe

---
+++ practice_08_JS_basics/scripts/main.js
@@ -0,0 +1,74 @@
+console.log("Hello world");
+
+//let age = +prompt("Enter your age");
+
+//    if (age < 21) {
+ //       console.log("welcome");
+        
+   // } 
+    //else {
+    //    console.log("Denied");  
+    //}
+
+   // let number = prompt("Add number");
+
+    //if (number % 2 === 0) {
+     //   console.log("odd number");
+    //} else {
+    //    console.log("not odd number");
+    //}
+
+   // const numb = +prompt("Введите число");
+
+     //if (numb % 2 === 0 && numb % 3 === 0) {
+    //  console.log("Кратно и 2 и 3");
+    // }
+    //  else if (numb % 2 === 0) {
+    //console.log("Кратно 2");
+   //} 
+   // else if (numb % 3 === 0) {
+   //  console.log("Кратно 3");
+  //} 
+   // else {
+    // console.log("Не кратно ни 2 ни 3");
+ //}
+
+
+//  let age = +prompt("enter age")
+
+ // if(age <= 21){
+ //   console.log("denied");
+ // }
+ //   else if(age >= 21 && age <= 45){
+ //   console.log("доступ открыт");
+ // }
+ //  else {
+ //   console.log("доступен ограничен");
+ // }
+
+ //let x = +prompt("enter age")
+  
+ //if(x< 21 || age >45){
+ //   console.log("accept");
+ //}
+
+// let x = +prompt("enter age")
+
+// if(!(x >=21  && x<=45)){
+ //   console.log("open");
+ //}
+//  const userName = prompt("enter your name");
+//  if (!userName || userName === "0") {
+ //   console.log("Имя неверно!"); }
+  
+    const userName = prompt('Write your name');
+  if (userName !== '0' && !userName) {
+    console.log("Name is wrong!");
+}
+
+  
+
+
+
+
+    
